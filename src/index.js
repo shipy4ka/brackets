43116a3dee6882ba74e38ea8f203d9f45b0fe8bf
shipy4ka <marinauglanica@gmail.com>
2023-02-20 17:01:01 +0300

    docs: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if((str.length === undefined) || (str.length === 1) || (str.length % 2 !== 0)) {
+    return false;
+  }
+
+  let stack = [];
+  let pairsOfBrackets = new Map(bracketsConfig); 
+   
+  for (let char of str) { 
+    if (pairsOfBrackets.get(stack.at(-1)) === char) { 
+      stack.pop(); 
+    } else { 
+      stack.push(char); 
+    }
+  }
+  return stack.length === 0; 
 }
